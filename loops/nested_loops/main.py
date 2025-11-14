86501372d1efabc3f2fa10da81f1ff64752ecfc7
npatel0925-blip <>
2025-11-14 23:14:37 +0000

    Nested loops

---
+++ loops/nested_loops/main.py
@@ -0,0 +1,7 @@
+produce = ["Tomatoes", "Lettuce"]
+dairy = ["Milk", "Cheese"]
+groceries = [produce,dairy]
+for section in groceries:
+    for item in section:
+        print(f" - {item}")
+    print("")
